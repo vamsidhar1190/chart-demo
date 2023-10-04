8cb10a4cdd2191c4b4064258192e8710f96bc177
vamsi <vamsidhar1190@gmail.com>
2023-10-04 17:40:08 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; // Import Link from react-router-dom
+import Charts from "./Charts";
+import Home from "./components/Home";
+import Weather from "./components/Weather";
+import LineChart from "./components/Linegraph";
+import Navbar from "./navbar";
+// import WeatherBarGraph from './WeatherBarGraph';
+// import Product from "./Weather";
+// import './app.css';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <div>
+      <div>
+    <BrowserRouter>
+     <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/charts" element={<LineChart />} />
+        <Route path="/weather" element={<Weather />} />
+        <Route path="/line" element={<LineChart />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+ 
+  </div>
   );
 }
 
